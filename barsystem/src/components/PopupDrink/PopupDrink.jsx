e55890485b81f2563395427184262b3718ebb9e4
ivandjuka77 <captainsubzer@gmail.com>
2022-12-01 14:48:22 +0100

    Removed use effect.

--- barsystem/src/components/PopupDrink/PopupDrink.jsx
+++ barsystem/src/components/PopupDrink/PopupDrink.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { drinksSold, soldSum } from '../../features/soldSum/soldSumSlice';
 import { returnDrinks } from '../../features/drinks/drinksSlice';
